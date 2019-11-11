ae2f3ca5f0b359c2dc77d0371d6a7b174bffaece
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-11-11 16:00:39 -0500

    Fixes remove_from_... methods on DLLs to check for Nones

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -66,9 +66,10 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        value = self.head.value
-        self.delete(self.head)
-        return value
+        if self.head is not None:
+            value = self.head.value
+            self.delete(self.head)
+            return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -88,9 +89,10 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        value = self.tail.value
-        self.delete(self.tail)
-        return value
+        if self.tail is not None:
+            value = self.tail.value
+            self.delete(self.tail)
+            return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -125,7 +127,7 @@ class DoublyLinkedList:
             node.delete()
         else:
             node.delete()
-        
+
     """Returns the highest value currently in the list"""
     def get_max(self):
         max_value = self.head.value
