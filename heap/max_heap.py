24963754fd242dd69c0a58e19d42835d0f67d57d
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-11-14 16:08:27 -0500

    Should complete max_heap's _bubble_up & _sift_down methods

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,7 +3,7 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
 
     def delete(self):
         pass
@@ -12,10 +12,20 @@ class Heap:
         pass
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        p_index = (index - 1) // 2
+        if self.storage[p_index] < self.storage[index]:
+            self.storage[p_index], self.storage[index] = self.storage[index], self.storage[p_index]
+            self._bubble_up(p_index)
 
     def _sift_down(self, index):
-        pass
+        l_index = (index * 2) + 1
+        r_index = (index * 2) + 2
+        if self.storage[l_index] > self.storage[index]:
+            self.storage[l_index], self.storage[index] = self.storage[index], self.storage[l_index]
+            self._sift_down(l_index)
+        elif self.storage[r_index] > self.storage[index]:
+            self.storage[r_index], self.storage[index] = self.storage[index], self.storage[r_index]
+            self._sift_down(r_index)
